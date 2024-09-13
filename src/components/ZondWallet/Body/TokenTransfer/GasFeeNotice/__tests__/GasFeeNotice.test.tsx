b01a24523f1c2fbed0be853d51d948a7380ecc55
Shaheer Shukur <shaheershukur@gmail.com>
2024-09-13 05:30:38 +0400

    test: updates gas fee test case

--- src/components/ZondWallet/Body/TokenTransfer/GasFeeNotice/__tests__/GasFeeNotice.test.tsx
+++ src/components/ZondWallet/Body/TokenTransfer/GasFeeNotice/__tests__/GasFeeNotice.test.tsx
@@ -89,4 +89,45 @@ describe("GasFeeNotice", () => {
       screen.getByText("Estimated gas fee is 2.64 QRL"),
     ).toBeInTheDocument();
   });
+
+  it("should display the estimated gas fee for ERC 20 token", async () => {
+    renderComponent(
+      mockedStore({
+        zondStore: {
+          getErc20TokenGas: jest.fn(
+            async (
+              from: string,
+              to: string,
+              value: number,
+              contractAddress: string,
+              decimals: number,
+            ) => {
+              from;
+              to;
+              value;
+              contractAddress;
+              decimals;
+              return "5.2564854";
+            },
+          ),
+        },
+      }),
+      {
+        isErc20Token: true,
+        tokenContractAddress: "0x28c4113a9d3a2e836f28c23ed8e3c1e7c243f566",
+        tokenDecimals: 18,
+        isSubmitting: false,
+        from: "0x205046e6A6E159eD6ACedE46A36CAD6D449C80A1",
+        to: "0x20fB08fF1f1376A14C055E9F56df80563E16722b",
+        value: 1.45,
+      },
+    );
+
+    await act(async () => {
+      expect(screen.getByText("Estimating gas fee")).toBeInTheDocument();
+    });
+    expect(
+      screen.getByText("Estimated gas fee is 5.2564 QRL"),
+    ).toBeInTheDocument();
+  });
 });
