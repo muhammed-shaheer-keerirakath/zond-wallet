fbc3ff05cd0073ff936e2f57ab8c51dcf7b41190
Shaheer Shukur <shaheershukur@gmail.com>
2024-09-24 20:31:23 +0400

    feat: adds background script

---
+++ scripts/background.ts
@@ -0,0 +1,23 @@
+import browser from "webextension-polyfill";
+
+console.log("Zond Wallet: Background service worker script running!");
+
+const initiateBackgroundScript = async () => {
+  await browser.scripting.registerContentScripts([
+    {
+      id: "zondContentScript",
+      matches: ["<all_urls>"],
+      js: ["scripts/contentScript.js"],
+      runAt: "document_start",
+      allFrames: true,
+      // @ts-expect-error.
+      // This is important. The script must run in the "MAIN" world,
+      // so that the zond provider will be available browser wide, not just isolated to the extension.
+      world: "MAIN",
+    },
+  ]);
+};
+
+// This is the starting point of script execution of zond wallet.
+// This file is set as an entry in the "background" section of the manifest file.
+initiateBackgroundScript();
