2cb1dfd6b6ceb78d1e72c2c899d4879e343c87c8
Shaheer Shukur <shaheershukur@gmail.com>
2024-10-02 03:57:07 +0400

    feat: exports requestProvider

--- src/wallet-provider/index.ts
+++ src/wallet-provider/index.ts
@@ -1,4 +1,5 @@
 import { WindowPostMessageStream } from "./post-message-stream";
 import { initializeProvider } from "./providers";
+import { requestProvider } from "./providers/EIP6963";
 
-export { initializeProvider, WindowPostMessageStream };
+export { initializeProvider, requestProvider, WindowPostMessageStream };
