df338a7982390863a88df3f1305162c500c29831
Shaheer Shukur <shaheershukur@gmail.com>
2024-10-03 03:43:10 +0400

    feat: adds note for requestprovider

--- src/wallet-provider/index.ts
+++ src/wallet-provider/index.ts
@@ -2,4 +2,9 @@ import { WindowPostMessageStream } from "./post-message-stream";
 import { initializeProvider } from "./providers";
 import { requestProvider } from "./providers/EIP6963";
 
-export { initializeProvider, requestProvider, WindowPostMessageStream };
+export {
+  initializeProvider,
+  // This function is intended to be used by a dapp.
+  requestProvider,
+  WindowPostMessageStream,
+};
