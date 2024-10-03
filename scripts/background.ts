e53566ffd7c0f4a89f617510a3a5fe2cf9a132d0
Shaheer Shukur <shaheershukur@gmail.com>
2024-10-03 04:38:08 +0400

    feat: updates extension scripts

--- scripts/background.ts
+++ scripts/background.ts
@@ -1,21 +1,37 @@
 import browser from "webextension-polyfill";
 
-const initiateBackgroundScript = async () => {
-  await browser.scripting.registerContentScripts([
-    {
-      id: "zondContentScript",
-      matches: ["<all_urls>"],
-      js: ["scripts/contentScript.js"],
-      runAt: "document_start",
-      allFrames: true,
-      // @ts-expect-error.
-      // This is important. The script must run in the "MAIN" world,
-      // so that the zond provider will be available browser wide, not just isolated to the extension.
-      world: "MAIN",
-    },
-  ]);
+type ContentScriptEntry = browser.Scripting.RegisteredContentScript;
+
+const initializeBackgroundScript = async () => {
+  try {
+    const previouslyRegisteredScriptIds = (
+      await browser.scripting.getRegisteredContentScripts()
+    ).map((script) => script.id);
+    const contentScripts: ContentScriptEntry[] = [
+      {
+        id: "zondContentScript",
+        matches: ["<all_urls>"],
+        js: ["scripts/contentScript.js"],
+        runAt: "document_start",
+        allFrames: true,
+        // @ts-expect-error.
+        // This is important. The script must run in the "MAIN" world,
+        // so that the zond provider will be available browser wide, not just isolated to the extension.
+        world: "MAIN",
+      },
+    ];
+
+    // This registers the content scripts to browser pages, if not already done.
+    await browser.scripting.registerContentScripts(
+      contentScripts.filter(
+        (script) => !previouslyRegisteredScriptIds.includes(script.id),
+      ),
+    );
+  } catch (error) {
+    console.warn("Zond Wallet: Failed to register the content scripts", error);
+  }
 };
 
 // This is the starting point of script execution of zond wallet.
 // This file is set as an entry in the "background" section of the manifest file.
-initiateBackgroundScript();
+initializeBackgroundScript();
--- scripts/contentScript.ts
+++ scripts/contentScript.ts
@@ -5,7 +5,7 @@ import {
   ZOND_WALLET_PROVIDER_INFO,
 } from "./constants";
 
-const initiateZondContentScript = () => {
+const initializeContentScript = () => {
   try {
     const zondStream = new WindowPostMessageStream({
       name: ZOND_POST_MESSAGE_STREAM.NAME,
@@ -26,4 +26,4 @@ const initiateZondContentScript = () => {
 };
 
 // This function accounces the zond provider(based on EIP-6963) to be listened by the dApps.
-initiateZondContentScript();
+initializeContentScript();
