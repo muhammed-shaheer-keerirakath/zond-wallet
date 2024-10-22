8858e5662e7e4181dcbc2a4bbfa66573a79d6ba9
Shaheer Shukur <shaheershukur@gmail.com>
2024-10-22 19:05:22 +0400

    feat: adds dapp request website component

---
+++ src/components/ZondWallet/DAppRequest/DAppRequestDetails/DAppRequestWebsite/DAppRequestWebsite.tsx
@@ -0,0 +1,28 @@
+import { Card } from "@/components/UI/Card";
+import { DAppRequestType } from "@/scripts/middlewares/connectWalletMiddleware";
+
+type DAppRequestWebsiteProps = {
+  dAppRequestData: DAppRequestType;
+};
+
+const DAppRequestWebsite = ({ dAppRequestData }: DAppRequestWebsiteProps) => {
+  return (
+    <Card className="flex gap-4 p-4">
+      <img
+        className="h-8"
+        src={dAppRequestData?.requestData?.senderData?.favIconUrl}
+        alt={dAppRequestData?.requestData?.senderData?.title}
+      />
+      <div className="flex flex-col gap-1">
+        <span className="font-bold">
+          {dAppRequestData?.requestData?.senderData?.url}
+        </span>
+        <span className="text-xm opacity-80">
+          {dAppRequestData?.requestData?.senderData?.title}
+        </span>
+      </div>
+    </Card>
+  );
+};
+
+export default DAppRequestWebsite;
