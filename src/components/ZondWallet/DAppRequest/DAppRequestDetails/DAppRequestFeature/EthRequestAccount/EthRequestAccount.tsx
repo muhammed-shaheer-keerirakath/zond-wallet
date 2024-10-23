453391e1cfb16921080bf83ce98978981e04865b
Shaheer Shukur <shaheershukur@gmail.com>
2024-10-23 10:17:41 +0400

    feat: adds eth request account component

---
+++ src/components/ZondWallet/DAppRequest/DAppRequestDetails/DAppRequestFeature/EthRequestAccount/EthRequestAccount.tsx
@@ -0,0 +1,57 @@
+import { Checkbox } from "@/components/UI/Checkbox";
+import AccountId from "@/components/ZondWallet/Body/AccountList/AccountId/AccountId";
+import { useStore } from "@/stores/store";
+import { observer } from "mobx-react-lite";
+import { useEffect, useState } from "react";
+
+type EthRequestAccountProps = {
+  addToResponseData: (data: any) => void;
+};
+
+const EthRequestAccount = observer(
+  ({ addToResponseData }: EthRequestAccountProps) => {
+    const { zondStore } = useStore();
+    const { activeAccount } = zondStore;
+    const account = activeAccount?.accountAddress;
+
+    const [response, setResponse] = useState<{ accounts: string[] }>({
+      accounts: [],
+    });
+
+    useEffect(() => {
+      addToResponseData({ ...response });
+    }, [response]);
+
+    return (
+      <div className="flex flex-col gap-4">
+        <div className="text-sm font-bold">
+          Connect your zond wallet account
+        </div>
+        {account ? (
+          <div className="flex items-start space-x-3">
+            <Checkbox
+              id="account"
+              onCheckedChange={(checked) => {
+                if (checked) {
+                  setResponse({ accounts: [account] });
+                } else {
+                  setResponse({ accounts: [] });
+                }
+              }}
+            />
+            <label
+              htmlFor="account"
+              className="cursor-pointer text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              <AccountId account={account} />
+            </label>
+          </div>
+        ) : (
+          <div>Account not available to connect</div>
+        )}
+      </div>
+    );
+  },
+);
+
+export default EthRequestAccount;
