5815d522c4cacf0a1e79152ab86429deccf291c9
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2025-02-19 13:35:56 +0400

    feat: updates zond config

--- src/configuration/zondConfig.ts
+++ src/configuration/zondConfig.ts
@@ -1,5 +1,5 @@
 export const ZOND_PROVIDER = {
-  DEV: { id: "DEV", url: "http://localhost:8545", name: "Zond Local Node" },
+  DEV: { id: "DEV", url: "http://127.0.0.1:32774", name: "Zond Local Node" },
   TEST_NET: {
     id: "TEST_NET",
     url: "http://209.250.255.226:8545",
