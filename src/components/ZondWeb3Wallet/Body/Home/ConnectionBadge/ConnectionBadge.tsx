51b364fbc91adfa10424ebded3240388ff59ba59
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2025-02-19 20:21:55 +0400

    feat: adds form validation

--- src/components/ZondWeb3Wallet/Body/Home/ConnectionBadge/ConnectionBadge.tsx
+++ src/components/ZondWeb3Wallet/Body/Home/ConnectionBadge/ConnectionBadge.tsx
@@ -9,13 +9,17 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/UI/Dialog";
+import { Form } from "@/components/UI/Form";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/UI/tabs";
 import { BlockchainType, ZOND_PROVIDER } from "@/configuration/zondConfig";
 import { useStore } from "@/stores/store";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { cva } from "class-variance-authority";
 import { HardDrive, Network, PlugZap, Workflow, X } from "lucide-react";
 import { observer } from "mobx-react-lite";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 const networkStatusClasses = cva("h-2 w-2 rounded-full", {
   variants: {
@@ -44,6 +48,20 @@ type ConnectionBadgeProps = {
   isDisabled?: boolean;
 };
 
+const FormSchema = z.object({
+  ipAddress: z.string().min(1, "IP address is required"),
+  port: z.string().min(1, "Port is required"),
+});
+
+const getFormSchema = (isValidationRequired: boolean) => {
+  return isValidationRequired
+    ? z.object({
+        ipAddress: z.string().min(1, "IP address is required"),
+        port: z.string().min(1, "Port is required"),
+      })
+    : z.object({});
+};
+
 const ConnectionBadge = observer(
   ({ isDisabled = false }: ConnectionBadgeProps) => {
     const { zondStore } = useStore();
@@ -51,83 +69,119 @@ const ConnectionBadge = observer(
     const { isConnected, blockchain } = zondConnection;
 
     const [selectedBlockchain, setSelectedBlockchain] = useState(blockchain);
+    const [isValidationRequired, setIsValidationRequired] = useState(
+      ZOND_PROVIDER[selectedBlockchain].isConfigurationRequired,
+    );
+
+    useEffect(() => {
+      setIsValidationRequired(
+        ZOND_PROVIDER[selectedBlockchain].isConfigurationRequired,
+      );
+    }, [selectedBlockchain]);
+
+    useEffect(() => {
+      reset({});
+    }, [isValidationRequired]);
+
+    async function onSubmit(formData: z.infer<typeof FormSchema>) {
+      console.log(">>>formData", formData);
+      selectBlockchain(selectedBlockchain);
+    }
+
+    const form = useForm<z.infer<typeof FormSchema>>({
+      resolver: zodResolver(getFormSchema(isValidationRequired)),
+      mode: "onChange",
+      reValidateMode: "onSubmit",
+      defaultValues: {
+        ipAddress: "",
+        port: "",
+      },
+    });
+    const {
+      handleSubmit,
+      reset,
+      formState: { isSubmitting, isValid },
+    } = form;
 
     return (
-      <Dialog>
-        <DialogTrigger asChild disabled={isDisabled}>
-          <Button
-            variant="outline"
-            className="flex items-center gap-2 rounded-full px-4 py-2 text-foreground"
-          >
-            <Card
-              className={networkStatusClasses({
-                networkStatus: isConnected,
-              })}
-            />
-            {ZOND_PROVIDER[blockchain].name}
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="w-80 rounded-md">
-          <DialogHeader className="text-left">
-            <DialogTitle>Select Blockchain</DialogTitle>
-          </DialogHeader>
-          <div>
-            <Tabs
-              defaultValue={blockchain}
-              onValueChange={(value) => {
-                setSelectedBlockchain(value as BlockchainType);
-              }}
-            >
-              <TabsList className="grid w-full grid-cols-3">
-                {Object.values(ZOND_PROVIDER).map((provider) => {
-                  return (
-                    <TabsTrigger
-                      key={provider.id}
-                      value={provider.id}
-                      className="data-[state=active]:text-secondary"
-                    >
-                      {getConnectionTypeIcon(provider.id)}
-                      <span className="text-xs">{provider.name}</span>
-                    </TabsTrigger>
-                  );
-                })}
-              </TabsList>
-              {Object.values(ZOND_PROVIDER).map((provider) => {
-                return (
-                  <TabsContent key={provider.id} value={provider.id}>
-                    <Card className="p-2">
-                      <div className="space-y-2">
-                        <div>{provider.description}</div>
-                        {provider.isConfigurationRequired && (
-                          <div>the form for ip and port</div>
-                        )}
-                      </div>
-                    </Card>
-                  </TabsContent>
-                );
-              })}
-            </Tabs>
-          </div>
-          <DialogFooter className="flex flex-row gap-4">
-            <DialogClose asChild>
-              <Button className="w-full" type="button" variant="outline">
-                <X className="mr-2 h-4 w-4" />
-                Cancel
+      <Form {...form}>
+        <form name="connectionBadgeForm" aria-label="connectionBadgeForm">
+          <Dialog>
+            <DialogTrigger asChild disabled={isDisabled}>
+              <Button
+                variant="outline"
+                className="flex items-center gap-2 rounded-full px-4 py-2 text-foreground"
+              >
+                <Card
+                  className={networkStatusClasses({
+                    networkStatus: isConnected,
+                  })}
+                />
+                {ZOND_PROVIDER[blockchain].name}
               </Button>
-            </DialogClose>
-            <Button
-              className="w-full"
-              type="button"
-              onClick={() => {
-                selectBlockchain(selectedBlockchain);
-              }}
-            >
-              <PlugZap className="mr-2 h-4 w-4" />
-              Connect
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+            </DialogTrigger>
+            <DialogContent className="w-80 rounded-md">
+              <DialogHeader className="text-left">
+                <DialogTitle>Select Blockchain</DialogTitle>
+              </DialogHeader>
+              <div>
+                <Tabs
+                  defaultValue={blockchain}
+                  onValueChange={(value) => {
+                    setSelectedBlockchain(value as BlockchainType);
+                  }}
+                >
+                  <TabsList className="grid w-full grid-cols-3">
+                    {Object.values(ZOND_PROVIDER).map((provider) => {
+                      return (
+                        <TabsTrigger
+                          key={provider.id}
+                          value={provider.id}
+                          className="data-[state=active]:text-secondary"
+                        >
+                          {getConnectionTypeIcon(provider.id)}
+                          <span className="text-xs">{provider.name}</span>
+                        </TabsTrigger>
+                      );
+                    })}
+                  </TabsList>
+                  {Object.values(ZOND_PROVIDER).map((provider) => {
+                    return (
+                      <TabsContent key={provider.id} value={provider.id}>
+                        <Card className="p-2">
+                          <div className="space-y-2">
+                            <div>{provider.description}</div>
+                            {provider.isConfigurationRequired && (
+                              <div>the form for ip and port</div>
+                            )}
+                          </div>
+                        </Card>
+                      </TabsContent>
+                    );
+                  })}
+                </Tabs>
+              </div>
+              <DialogFooter className="flex flex-row gap-4">
+                <DialogClose asChild>
+                  <Button className="w-full" type="button" variant="outline">
+                    <X className="mr-2 h-4 w-4" />
+                    Cancel
+                  </Button>
+                </DialogClose>
+                <Button
+                  className="w-full"
+                  type="button"
+                  disabled={isValidationRequired && (isSubmitting || !isValid)}
+                  onClick={handleSubmit(onSubmit)}
+                >
+                  <PlugZap className="mr-2 h-4 w-4" />
+                  Connect
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </form>
+      </Form>
     );
   },
 );
