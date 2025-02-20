03e5f06df652ee1e83f9356ba28b30bd1af3ff53
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2025-02-20 09:32:43 +0400

    feat: updates address util

--- src/functions/getSplitAddress.ts
+++
@@ -1,8 +0,0 @@
-export const getSplitAddress = (address: string) => {
-  const prefix = address?.substring(0, 2);
-  const addressSplit: string[] = [];
-  for (let i = 2; i < address?.length; i += 5) {
-    addressSplit.push(address?.substring(i, i + 5));
-  }
-  return { prefix, addressSplit };
-};
--- src/utilities/stringUtil.ts
+++ src/utilities/stringUtil.ts
@@ -5,14 +5,14 @@ class StringUtil {
   /**
    * A function for splitting the address with spaces between them, making the address more readable.
    */
-  static getSplitAddress(accountAddress: string, splitLength: number = 4) {
-    const prefix = accountAddress.substring(0, 2);
-    const idSplit: string[] = [];
-    for (let i = 2; i < accountAddress.length; i += splitLength) {
-      idSplit.push(accountAddress.substring(i, i + splitLength));
+  static getSplitAddress(accountAddress: string, splitLength: number = 5) {
+    const prefix = accountAddress.substring(0, 1);
+    const addressSplit: string[] = [];
+    for (let i = 1; i < accountAddress.length; i += splitLength) {
+      addressSplit.push(accountAddress.substring(i, i + splitLength));
     }
 
-    return [prefix, ...idSplit].join(" ");
+    return { prefix, addressSplit };
   }
 }
 
