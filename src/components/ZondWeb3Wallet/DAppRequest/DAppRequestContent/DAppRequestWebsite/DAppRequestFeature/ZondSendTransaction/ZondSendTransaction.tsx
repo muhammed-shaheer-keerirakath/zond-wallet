08ff85f614627ac85299ef2ca1a38933e466c1d4
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2025-05-16 11:42:14 +0400

    refactor: uses state

--- src/components/ZondWeb3Wallet/DAppRequest/DAppRequestContent/DAppRequestWebsite/DAppRequestFeature/ZondSendTransaction/ZondSendTransaction.tsx
+++ src/components/ZondWeb3Wallet/DAppRequest/DAppRequestContent/DAppRequestWebsite/DAppRequestFeature/ZondSendTransaction/ZondSendTransaction.tsx
@@ -1,6 +1,7 @@
 import { useStore } from "@/stores/store";
 import { observer } from "mobx-react-lite";
 import ZondSendTransactionForContent from "./ZondSendTransactionForContent/ZondSendTransactionForContent";
+import { useEffect, useState } from "react";
 
 export const SEND_TRANSACTION_TYPES = {
   CONTRACT_DEPLOYMENT: "CONTRACT_DEPLOYMENT",
@@ -13,30 +14,32 @@ const ZondSendTransaction = observer(() => {
   const { dAppRequestStore } = useStore();
   const { dAppRequestData } = dAppRequestStore;
 
-  let transactionHeading = "";
-  let transactionSubHeading = "";
-  const transactionType = getTransactionType(dAppRequestData?.params[0]);
+  const [transactionHeading, setTransactionHeading] = useState("");
+  const [transactionSubHeading, setTransactionSubHeading] = useState("");
+  const [transactionType, setTransactionType] = useState<
+    keyof typeof SEND_TRANSACTION_TYPES
+  >(SEND_TRANSACTION_TYPES.UNKNOWN);
 
-  function getTransactionType(params: any) {
+  useEffect(() => {
+    const params = dAppRequestData?.params[0];
     if (!params || typeof params !== "object") {
-      return SEND_TRANSACTION_TYPES.UNKNOWN;
+      return;
     }
     const { to, value, data } = params;
     if (!to && data) {
-      transactionHeading = "Deploy a contract";
-      transactionSubHeading = "This site wants to deploy a contract";
-      return SEND_TRANSACTION_TYPES.CONTRACT_DEPLOYMENT;
+      setTransactionHeading("Deploy a contract");
+      setTransactionSubHeading("This site wants to deploy a contract");
+      setTransactionType(SEND_TRANSACTION_TYPES.CONTRACT_DEPLOYMENT);
     } else if (to && data) {
-      transactionHeading = "Interact with a contract";
-      transactionSubHeading = "This site wants to interact with a contract";
-      return SEND_TRANSACTION_TYPES.CONTRACT_INTERACTION;
+      setTransactionHeading("Interact with a contract");
+      setTransactionSubHeading("This site wants to interact with a contract");
+      setTransactionType(SEND_TRANSACTION_TYPES.CONTRACT_INTERACTION);
     } else if (to && value && !data) {
-      transactionHeading = "Transfer ZND";
-      transactionSubHeading = "This site wants to send ZND";
-      return SEND_TRANSACTION_TYPES.ZND_TRANSFER;
+      setTransactionHeading("Transfer ZND");
+      setTransactionSubHeading("This site wants to send ZND");
+      setTransactionType(SEND_TRANSACTION_TYPES.ZND_TRANSFER);
     }
-    return SEND_TRANSACTION_TYPES.UNKNOWN;
-  }
+  }, [dAppRequestData]);
 
   return (
     <div className="flex flex-col gap-4">
